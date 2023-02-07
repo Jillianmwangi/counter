4c1b9cc2a513017eceba9ec1ee1cb5b3a08a19cd
Jillianmwangi <mwangyjill@gmail.com>
2023-02-07 13:39:08 +0300

    add files

---
+++ counterz/src/components/counter.jsx
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+
+export class Counter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { count: 0 };
+    this.handleIncrement = this.handleIncrement.bind(this);
+    this.handleDecrement = this.handleDecrement.bind(this);
+  }
+  handleDecrement() {
+    this.setState((prev) => {
+     return {count:prev.count - 1}
+    });
+
+  }
+  handleIncrement() {
+    this.setState((prev) => {
+     return {count:prev.count + 1}
+    });
+  }
+
+  render() {
+    return (
+      <div className="counter">
+        <span className="value">{this.state.count}</span>
+        <span className="btns">
+          <button onClick={this.handleIncrement}> + </button>
+          <button onClick={this.handleDecrement}> - </button>
+        </span>
+      </div>
+    );
+  }
+}
+
+export default Counter;
